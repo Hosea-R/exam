84f75a34c171e100d025af44df372e077f14d4be
mrazanad <mrazanad@c1r9s13.42antananarivo.mg>
2024-12-05 18:51:09 +0300

    last update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,93 +1,60 @@
 #include "ft_printf.h"
 
-int ft_print_string(char *str)
+static int ft_putstr(char *s)
 {
-    int length = 0;
-    if (!str) str = "(null)";
-    for (size_t i = 0; str[i]; i++)
+    int len = 0;
+    if (!s)
+        s = "(null)";
+    while (s[len])
+        len++;
+    return (write(1, s, len));
+}
+
+static int ft_putnbr_base(long n, char *base, int base_len)
+{
+    char buf[20];
+    int i = 0, len = 0;
+
+    if (n == 0)
+        return (write(1, "0", 1));
+    if (n < 0)
     {
-        write(1, &str[i], 1);
-        length++;
+        len += write(1, "-", 1);
+        n = -n;
     }
-    return length;
-}
-
-void ft_putnbr(int d)
-{
-    if (d == -2147483648)
+    while (n > 0)
     {
-        write(1, "-2147483648", 11);
-        return;
+        buf[i++] = base[n % base_len];
+        n /= base_len;
     }
-    if (d < 0)
+    while (--i >= 0)
+        len += write(1, &buf[i], 1);
+    return (len);
+}
+
+int ft_printf(const char *fmt, ...)
+{
+    va_list args;
+    int len = 0;
+
+    va_start(args, fmt);
+    while (*fmt)
     {
-        write(1, "-", 1);
-        d = -d;
-    }
-    if (d > 9)
-        ft_putnbr(d / 10);
-    char c = d % 10 + '0';
-    write(1, &c, 1);
-}
-
-int ft_print_int(int d)
-{
-    int length = 0;
-    int n = d;
-    if (d <= 0) length++;
-    while (n && ++length) n /= 10;
-    ft_putnbr(d);
-    return length;
-}
-
-void ft_puthex(unsigned int x)
-{
-    char *hex = "0123456789abcdef";
-    if (x > 15)
-        ft_puthex(x / 16);
-    char c = hex[x % 16];
-    write(1, &c, 1);    
-}
-
-int ft_print_hex(unsigned int x)
-{
-    int length = 1;
-    unsigned int u = x;
-    while (u >>= 4)
-        length++;
-    ft_puthex(x);
-    return length;    
-}
-
-int ft_conversions(int c, va_list ap)
-{
-    if (c == 's') return ft_print_string(va_arg(ap, char *));
-    if (c == 'd') return ft_print_int(va_arg(ap, int));
-    if (c == 'x') return ft_print_hex(va_arg(ap, unsigned int));
-    return 0;
-}
-
-int ft_printf(const char *str, ...)
-{
-    va_list ap;
-    int length = 0;
-    va_start(ap, str);
-    for (int i = 0; str[i]; i++)
-    {
-        if (str[i] == '%' && str[++i])
-            length += ft_conversions(str[i], ap);
-        else
+        if (*fmt == '%' && (*(fmt + 1) == 's' || *(fmt + 1) == 'd' || *(fmt + 1) == 'x'))
         {
-            write(1, &str[i], 1);
-            length++;
+            fmt++;
+            if (*fmt == 's')
+                len += ft_putstr(va_arg(args, char *));
+            else if (*fmt == 'd')
+                len += ft_putnbr_base(va_arg(args, int), "0123456789", 10);
+            else if (*fmt == 'x')
+                len += ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef", 16);
         }
+        else
+            len += write(1, fmt, 1);
+        fmt++;
     }
-    va_end(ap);
-    return length;
+    va_end(args);
+    return (len);
 }
 
-int main(void)
-{
-    ft_printf("%d\n", 42);
-    return (0);
-}
--- get_next_line/exer.c
+++ get_next_line/exer.c
@@ -31,24 +31,24 @@ char *get_next_line(int fd)
     return (line);
 }
 
-int	main(void)
-{
-	int		fd;
-	char	*next_line;
-	int		count;
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*next_line;
+// 	int		count;
 
-	count = 0;
-	fd = open("test.txt", O_RDONLY);
-	while (1)
-	{
-		next_line = get_next_line(fd);
-		if (next_line == NULL)
-			break ;
-		count++;
-		printf("[%d] : %s\n", count, next_line);
-		free(next_line);
-		next_line = NULL;
-	}
-	close(fd);
-	return (0);
-}
+// 	count = 0;
+// 	fd = open("test.txt", O_RDONLY);
+// 	while (1)
+// 	{
+// 		next_line = get_next_line(fd);
+// 		if (next_line == NULL)
+// 			break ;
+// 		count++;
+// 		printf("[%d] : %s\n", count, next_line);
+// 		free(next_line);
+// 		next_line = NULL;
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
