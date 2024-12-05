c13cff4a741f99b1e97c33a60f13d774881a7f2f
mrazanad <mrazanad@c1r9s13.42antananarivo.mg>
2024-12-05 11:48:59 +0300

    update get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,135 +1,37 @@
 #include "get_next_line.h"
 
-int ft_strlen(char *str)
-{
-    int i = 0;
-    while (str[i])
-    {
-        i++;
-    }
-    return (i);
-}
-
-char *ft_strdup(char *str)
-{
-    char *dup = malloc(ft_strlen(str) + 1);
-    int i = 0;
-    while (str[i])
-    {
-        dup[i] = str[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
-}
-
-char *ft_strjoin(char *s1, char *s2)
-{
-    char *join = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-    int i = 0;
-    int j = 0;
-    while (s1[i])
-    {
-        join[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        join[i] = s2[j];
-        i++;
-        j++;
-    }
-    join[i] = '\0';
-    free(s1);
-    return (join);
-}
-
-char *ft_strrchr(char *all, int c)
-{
-    int i = ft_strlen(all) - 1;
-    while (i >= 0)
-    {
-        if (all[i] == (char)c)
-            return (all + i);
-        i--;
-    }
-    return (NULL);
-}
-
-char *ft_substr(char *str, int len)
-{
-    char *sub = malloc(len + 1);
-    int i = 0;
-    while (i < len)
-    {
-        sub[i] = str[i];
-        i++;
-    }
-    sub[i] = '\0';
-    return(sub);
-}
-
-char *ft_clean(char *all)
-{
-    char *new;
-    int i = 0;
-    while (all[i] && all[i] != '\n')
-        i++;
-    if (!all[i])
-    {
-        free(all);
-        return (NULL);
-    }
-    new = ft_strdup(all + i + 1);
-    free(all);
-    return (new);
-}
-
-char *ft_read(int fd, char *all)
-{
-    char *temp;
-    int i = BUFFER_SIZE;
-    while ((temp = malloc(BUFFER_SIZE + 1)))
-    {
-        i = read(fd, temp, BUFFER_SIZE);
-        if (i <= 0 || !temp[0])
-        {
-            free(temp);
-            return (i < 0 || !ft_strlen(all) ? (free(all), NULL) : all);
-        }
-        temp[i] = '\0';
-        all = ft_strjoin(all, temp);
-        if (ft_strrchr(all, 10))
-        {
-            free(temp);
-            return (all);
-        }
-        free(temp);
-    }
-    return (free(all), NULL);
-}
-
 char *get_next_line(int fd)
 {
-    static char* all;
-    char *line;
-    int i = 0;
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-    all = all ? all : ft_strdup("");
-    all = ft_read(fd, all);
-    if (!all)
+
+    char *buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
+
+    char *line = malloc(sizeof(char) * 10000);
+    if (!line)
     {
-        return(NULL);
+        free(buffer);
+        return (NULL);
     }
-    
-    while (all[i] && all[i] != '\n')
-        i++;
-    line = ft_substr(all, all[i] == '\n' ? i + 1 : i);
-    all = ft_clean(all);
+    char *current = line;
+    ssize_t bytes_read;
+    while ((bytes_read = read(fd, buffer, 1)) > 0)
+    {
+        *current++ = *buffer;
+        if (*buffer == '\n')
+            break;
+    }
+    free(buffer);
+    if (bytes_read < 0 || (bytes_read == 0 && current == line))
+    {
+        free(line);
+        return (NULL);
+    }
+    *current = '\0';
     return (line);
 }
-
 // int	main(void)
 // {
 // 	int		fd;
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,13 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-#include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 5
+#define BUFFER_SIZE 10000
 #endif
 
 char *get_next_line(int fd);
